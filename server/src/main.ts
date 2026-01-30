e213489d4a7a16edff9d8a40257a32ca0432a3e7
Jhoiner Martinez <153531218+Jhoiner243@users.noreply.github.com>
2026-01-30 16:19:08 -0500

    feat: add custom NestJS injection decorators and configure global validation pipe in main bootstrap.

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,23 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const logger = new Logger()
+  const globalPipe = new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+    transformOptions: {
+      enableImplicitConversion: true,
+    },
+  })
+
+  app.useGlobalPipes(globalPipe)
+
   await app.listen(process.env.PORT ?? 3000);
+  logger.log(`Main running on port ${process.env.PORT ?? 3000}`)
 }
 void bootstrap();
