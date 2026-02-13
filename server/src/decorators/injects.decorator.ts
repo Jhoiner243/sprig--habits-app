bb3f1bf135ff3e1f7372e56d196215a7a54e84af
Jhoiner Martinez <153531218+Jhoiner243@users.noreply.github.com>
2026-02-13 11:27:35 -0500

    feat: add habits logs for unique register for habit and add in main the swagger config

--- server/src/decorators/injects.decorator.ts
+++ server/src/decorators/injects.decorator.ts
@@ -1,11 +1,9 @@
 import { Inject, Injectable, InjectionToken } from '@nestjs/common';
 
-export class InjectsDecorators {
-  public static InjectableCustom() {
-    return Injectable();
-  }
+export function InjectableCustom () {
+  return Injectable()
+}
 
-  public static InjectCustom(token: InjectionToken) {
-    return Inject(token);
-  }
+export function InjectCustom (token: InjectionToken) {
+  return Inject(token)
 }
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,7 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -16,6 +16,17 @@ async function bootstrap() {
   })
 
   app.useGlobalPipes(globalPipe)
+
+  const config = new DocumentBuilder()
+  .setTitle('Api sprig - habit tracker')
+  .setDescription('This app is for keep the habits tracker')
+  .setVersion('1.0')
+  .build()
+
+  //The factory method SwaggerModule.createDocument() is used specifically to generate the Swagger document when you request it
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  
+  SwaggerModule.setup('api', app, documentFactory)
 
   await app.listen(process.env.PORT ?? 3000);
   logger.log(`Main running on port ${process.env.PORT ?? 3000}`)
